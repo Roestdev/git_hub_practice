b8859e58ab99220937e898c01a803ac3cbeca43d
roest_dev <github.ebook242@passinbox.com>
2026-01-27 15:52:19 +0100

    add code in branch DEV

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     // bases line in branch main
     // version 100.0.0
     println!("BASELINE");
+    // add code in branch dev
+        println!("add code in branch DEV");
+        println!("Version unchanged!");
 
 }
