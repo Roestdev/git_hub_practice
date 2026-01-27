e7e909e64c2577b3faf9e7dac9ab493485c56f74
roest_dev <github.ebook242@passinbox.com>
2026-01-27 16:13:52 +0100

    Set tag 100.0.1 in DEV

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ fn main() {
     // version 100.0.0
     println!("BASELINE");
     // add code in branch dev
-        println!("add code in branch DEV");
-        println!("Version unchanged!");
-
+    println!("add code in branch DEV");
+    println!("Version unchanged!");
+    // Set tag 100.0.1 in dev
+    println!("Set tag 100.0.1 in DEV");
 }
